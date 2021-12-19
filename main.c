94c9a5345af533bee314a645e417ab0e460b6753
Avishai Melamed <avishay2008@gmail.com>
2021-12-19 19:31:12 +0200

    this program works for 1 server, need to orgenize and try to run im linux

--- main.c
+++ main.c
@@ -11,6 +11,8 @@
 #define MAX_PORT_VAL 64000
 #define BACKLOG 10
 
+const char *request_end = '\r\n\r\n';
+
 int ranged_rand(int lower, int upper)
 {
     return (rand() % (upper - lower + 1)) + lower;
@@ -36,74 +38,98 @@ char* read_server_message(int server_socket){
     return returned_string;
 }
 
+int check_if_request_completed(char* request) {
+    int i = 0;
+    while(!((request[i] == '\r') && (request[i+1] =='\n') && (request[i+2] == '\r') && (request[i+3] =='\n'))) {
+        i++;
+        if (i == strlen(request)-1)
+            return 0;
+    }
+    return 1;
+}
+
 int main() {
-    int port, servers_array[3], client_connected; // the sockets returned from accept
+    int temp;
+    int port_server, port_client, servers_array[3], client_connected; // the sockets returned from accept
     int addr_len = sizeof(struct sockaddr_in);
-    char buff[1024], *full_request, *returned_string;
-    FILE* server_port_number = fopen('server_port.txt', 'w');
-    FILE* client_port_number = fopen('http_port.txt', 'w');
+    char buff[MAX_LEN_RECV], *full_request, *returned_string;
+    FILE *server_port_number, *client_port_number;
+    server_port_number = fopen("server_port.txt", "w");
+    client_port_number = fopen("http_port.txt", "w");
 
     //create a new socket for servers and for client
     int sock_client = socket(AF_INET, SOCK_STREAM, 0);
     int sock_server = socket(AF_INET, SOCK_STREAM, 0);
 
     //create sockaddr variable with random porn number
-    port = ranged_rand(MIN_PORT_VAL, MAX_PORT_VAL);
-    struct sockaddr_in service;
-    service.sin_family = AF_INET;
-    service.sin_addr.s_addr = inet_addr( "127.0.0.1" );
-    service.sin_port = htons(port);
+    port_server = ranged_rand(MIN_PORT_VAL, MAX_PORT_VAL);
+    port_client = ranged_rand(MIN_PORT_VAL, MAX_PORT_VAL);
+
+    struct sockaddr_in service_client, service_server;
+    service_client.sin_family = AF_INET;
+    service_client.sin_addr.s_addr = inet_addr( "127.0.0.1" );
+    service_client.sin_port = htons(port_client);
+
+    service_server.sin_family = AF_INET;
+    service_server.sin_addr.s_addr = inet_addr( "127.0.0.1" );
+    service_server.sin_port = htons(port_server);
 
     //bind sever socket, keep trying until success
-    while (bind(sock_server, (struct sockaddr*)&service, sizeof(service)) == -1) {
-        port = ranged_rand(MIN_PORT_VAL, MAX_PORT_VAL);
-        service.sin_port = htons(port);
+    while (bind(sock_server, (struct sockaddr*)&service_server, sizeof(service_server)) == -1) {
+        port_server = ranged_rand(MIN_PORT_VAL, MAX_PORT_VAL);
+        service_server.sin_port = htons(port_server);
     }
-    if (listen(sock_server, BACKLOG) != -1)
-        fprintf(server_port_number, "%d", port);
+    fprintf(server_port_number, "%d", port_server);
+    fclose(server_port_number);
+    if (listen(sock_server, BACKLOG) == -1)
+        exit(1);
 
 
     //bind client socket, keep trying until success
-    do {
-        port = ranged_rand(MIN_PORT_VAL, MAX_PORT_VAL);
-        service.sin_port = htons(port);
-    } while (bind(sock_client, (struct sockaddr*)&service, sizeof(service)) == -1);
-    if (listen(sock_client, BACKLOG) != -1)
-        fprintf(client_port_number, "%d", port);
+    while (bind(sock_client, (struct sockaddr*)&service_client, sizeof(service_client)) == -1) {
+        port_client = ranged_rand(MIN_PORT_VAL, MAX_PORT_VAL);
+        service_client.sin_port = htons(port_client);
+    }
+    fprintf(client_port_number, "%d", port_client);
+    fclose(client_port_number);
+    if (listen(sock_client, BACKLOG) == -1)
+        exit(1);
 
     // Waiting till 3 servers are connected
     int i = 0;
     while (i<3){
-        servers_array[i] = accept(sock_server, (struct sockaddr*)&service, &addr_len);
-        if(servers_array[i] > 0) {
+        servers_array[i] = accept(sock_server, (struct sockaddr*)&service_server, &addr_len);
+        if(servers_array[i] > 0 ) {
             i++;
             listen(sock_server, BACKLOG);
         }
         else{
-            printf("connection failed, try again");
+            printf("connection failed, exit");
+            exit(1);
         }
     }
 
     // Accepting http connection
-    client_connected = accept(sock_client, (struct sockaddr *) &service, &addr_len);
+    client_connected = accept(sock_client, (struct sockaddr *) &service_client, &addr_len);
     while(client_connected <= 0) { // failed to accept, try again
         listen(sock_client, BACKLOG);
-        client_connected = accept(sock_client, (struct sockaddr *) &service, &addr_len);
+        client_connected = accept(sock_client, (struct sockaddr *) &service_client, &addr_len);
     }
 
     i = 0;
     while (1){
 
-        while (strcmp(buff +strlen(buff) - 4, '\r\n\r\n')) {
+        do {
             recv(client_connected, buff, MAX_LEN_RECV, 0); // recives string from http untill \r\n\r\n
             full_request = strcat(full_request, buff);
-        }
+        }while (check_if_request_completed(full_request) == 0);
 
         send(servers_array[i], full_request, MAX_LEN_RECV, 0); //sends the string to next server by order
-        returned_string = read_server_message(servers_array[i]);
+        recv(servers_array[i], returned_string, MAX_LEN_RECV, 0);
+        //returned_string = read_server_message(temp);
         send(client_connected, returned_string, MAX_LEN_RECV, 0);
-        i = (1==3) ? 0 : i + 1;
-
+        i = (i==3) ? 0 : i + 1;
+        break;
         /* not sure if break is needed */
     }
 
